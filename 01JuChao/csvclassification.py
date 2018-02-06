f18098220fb9c2ce8cb4ae7cf7753265aa19f65a
randy pen <pencoa42@gmail.com>
2018-02-06 19:18:42 +0800

    share holders division

--- 01JuChao/csvclassification.py
+++ 01JuChao/csvclassification.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 
 with open('./data/share_person.csv', 'a') as csv_file:
@@ -23,4 +24,20 @@ with open('./data/share_company.csv', 'a') as csv_file:
 with open('./data/shareholder.csv') as csv_file:
     readCSV = csv.reader(csv_file, delimiter=',')
     for row in readCSV:
-        print(row[0])
+        if len(row[0]) > 4:
+            if '基金' in row[0] or 'FUND' in row[0]:
+                with open('./data/share_fund.csv', 'a') as csvfile:
+                    writer = csv.writer(csvfile, delimiter=',')
+                    writer.writerow(row)
+                continue
+            else:
+            # elif '公司' in row[0] or 'LIMITED' in row[0] or '有限合伙' in row[0] or 'BANK' in row[0] or 'UBSAG' in row[0]:
+                with open('./data/share_company.csv', 'a') as csvfile:
+                    writer = csv.writer(csvfile, delimiter=',')
+                    writer.writerow(row)
+                continue
+        if len(row[0]) <= 4:
+            with open('./data/share_person.csv', 'a') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',')
+                writer.writerow(row)
+            continue
