87ffc8bf49826a46f32fb446999e1b3a3e55b5bf
randy pen <pencoa42@gmail.com>
2018-03-21 22:49:16 +0800

    zhishime

--- 03EastMoney/getSPO.py
+++ 03EastMoney/getSPO.py
@@ -4,7 +4,7 @@ def finder(entity):
     client = MongoClient()
     db = client['QAdb']
     ans_rt = []
-    for u in db.posts.find({'head':entity}):
+    for u in db.zhishime.find({'head':entity}):
         ans_rt.append((u['relation'],u['tail']))
     return (entity,ans_rt)
 
@@ -13,5 +13,6 @@ if __name__ == '__main__':
     with open('./data/CompanySurvey/lawoffice.txt', 'r') as file:
         for line in file:
             spo = finder(line)
-            for u in spo:
-                print(u)
+            if len(spo) > 1:
+                for u in spo:
+                    print(u)
