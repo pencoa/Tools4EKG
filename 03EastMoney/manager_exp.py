0b490780ee4ae278f4e0df4cb014b14e6029e248
randy pen <pencoa42@gmail.com>
2018-04-03 14:22:54 +0800

    postag

--- 03EastMoney/manager_exp.py
+++ 03EastMoney/manager_exp.py
@@ -13,3 +13,13 @@ with open("./data/person_resume01.txt", 'a') as f:
     for exp in exps:
         seg_list = jieba.cut(exp, cut_all=False)
         f.write("\n".join(seg_list)+"\n")
+
+import jieba.posseg as pseg
+
+with open("./data/person_resume.txt", "r") as f:
+    for line in f:
+        text = line.strip()
+        words = pseg.cut(text)
+        for w in words:
+            with open("./data/person_resume02.txt", "a") as f1:
+                f1.write('%s %s\n' % (w.word, w.flag))
