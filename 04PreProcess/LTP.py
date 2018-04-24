81779b56c432b4f120f4c901986dda5a6fe464bf
randy pen <pencoa42@gmail.com>
2018-04-24 15:31:45 +0800

    finish men2entity

--- 04PreProcess/LTP.py
+++ 04PreProcess/LTP.py
@@ -14,11 +14,20 @@ params = {
 f = open('./data/experiences.csv', 'r')
 for line in f:
     line = line.strip()
-    params['text'] = line
-    response = requests.get(url_get_base, params=params)
-    with open('./data/ltp_experience.csv', 'a') as file:
-        file.write(response.content.decode('utf-8'))
-        file.write('\n\n')
+    if len(line) > 100:
+        line_list = line.split('。')
+        for i in line_list:
+            params['text'] = i
+            response = requests.get(url_get_base, params=params)
+            with open('./data/ltp_experience.csv', 'a') as file:
+                file.write(response.content.decode('utf-8'))
+                file.write('。\n')
+    else:
+        params['text'] = line
+        response = requests.get(url_get_base, params=params)
+        with open('./data/ltp_experience.csv', 'a') as file:
+            file.write(response.content.decode('utf-8'))
+            file.write('\n\n')
     sleep(0.5)
 
 f.close()
